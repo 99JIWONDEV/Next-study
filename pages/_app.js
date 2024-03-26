b523b54743ae21609fb053b360ff9f965761f9ae
99JIWONDEV <99jiwonpark@gmail.com>
2024-03-26 15:48:27 +0900

    [feat] section04 강의 수강

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+// 모든 페이지의 공통 설정들을 해주는 파일
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,15 @@
+export default function 나만의페이지(){
+  // 위쪽은 자바스크립트 작성하는 공간
+  const app = 3
+
+
+
+  // 아래쪽은 html 작성하는 공간
+  return (
+    <div>
+      <div>철수</div>
+      <button>클릭하세요</button>
+    </div>
+
+  )
+}
